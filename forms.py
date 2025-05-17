aec9c3aecbd6833338152a27fe4214c873c42e3b
conoros12 <conorosull05@gmail.com>
2025-05-17 11:13:33 +0100

    Made Filter form

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, DateTimeField, SubmitField
+from wtforms.validators import Optional
+
+class LogFilterForm(FlaskForm):
+    ip_address = StringField('Ip Address', validators=[Optional()])
+    status_code = SelectField('Status Code', choices=[
+        ('', 'All'),
+        ('200', '200 OK'),
+        ('404', '404 Not Found'),
+        ('500', '500 Internal Server Error'),
+        ('403', '403 Forbidden')
+    ], validators=[Optional()])
+
+    start_time = DateTimeField('Start Time (DD-MM-YY)', format='%d-%m-%y', validators=[Optional()])
+    end_time = DateTimeField('End Time (DD-MM-YY)', format = '%d-%m-%y', validators=[Optional()])
+    submit = SubmitField("Apply Filters")
