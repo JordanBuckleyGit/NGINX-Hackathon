55886a2e12c56111ced2809770ad8f15352f3778
JordanBuckleyGit <jordanbuckleycork@gmail.com>
2025-05-17 12:36:13 +0100

    got rid of extra col

--- schema.sql
+++ schema.sql
@@ -1,10 +1,16 @@
-CREATE TABLE logs (
+CREATE TABLE logs_new (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     ip_address TEXT,
     timestamp TEXT,
     request TEXT,
     status_code TEXT,
     bytes_sent INTEGER,
-    referrer TEXT,
     user_agent TEXT
 );
+
+INSERT INTO logs_new (id, ip_address, timestamp, request, status_code, bytes_sent, user_agent)
+SELECT id, ip_address, timestamp, request, status_code, bytes_sent, user_agent FROM logs;
+
+DROP TABLE logs;
+
+ALTER TABLE logs_new RENAME TO logs;
